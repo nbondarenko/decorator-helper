747b0d21841621cd119fdd98b1874b8707f1b626
nbondarenko <nataliya.bondarenko17@gmail.com>
2017-10-19 02:16:55 +0300

    Added CRUD pt2

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
+require('dotenv').config();
 var express = require('express');
 var app = express();
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const db = require('./config/db');
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+MongoClient.connect(db.url, (err, database) => {
+  if (err) return console.log(err);
+  require('./routes')(app, database);
+
+  app.listen(app.get('port'), () => {
+    console.log('We are live on ' + app.get('port'));
+  });
+})
+
 app.get('/', function (request, response) {
   response.send('Hello World!');
-});
-
-app.listen(app.get('port'), function () {
-  console.log('Example app listening on port', app.get('port'));
 });
